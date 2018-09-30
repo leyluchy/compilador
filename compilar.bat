1d19c8228eccfc54869461986c8d15fd152359f8
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2018-09-30 20:58:30 -0300

    Andando los comentarios multilinea, con dos niveles de profundidad

--- compilar.bat
+++ compilar.bat
@@ -2,5 +2,5 @@ c:\GnuWin32\bin\flex lexico.l
 pause
 c:\GnuWin32\bin\bison -dyv Sintactico.y
 pause
-C:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Primera.exe
+C:\TDM-GCC-64\bin\gcc.exe lex.yy.c y.tab.c -o Primera.exe
 pause
