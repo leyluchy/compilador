10ff62f91db2ec811d8d677498a4ee9bbe31019b
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-10-13 23:41:50 -0300

    Tercetos separados

--- tercetos.h
+++ tercetos.h
@@ -6,6 +6,7 @@
 #define OFFSET TAMANIO_TABLA
 #define MAX_TERCETOS 512
 
+/* Operadores extra para usar con los tokens */
 #define NOOP -1 /* Sin operador */
 #define BLOQ 7 /* Operador que indica el orden de las sentencias */
 #define CMP 21 /* Comparador de assembler */
@@ -16,10 +17,12 @@
 #define BEQ 14 /* != */
 #define BGT 8 /* <= */
 
+/* Posiciones dentro de un terceto */
 #define OP1 2
 #define OP2 3
 #define OPERADOR 1
 
+/* Funciones */
 int crear_terceto(int operador, int op1, int op2);
 void guardarTercetos();
 void modificarTerceto(int indice, int posicion, int valor);
@@ -29,5 +32,9 @@ typedef struct{
   int op1;
   int op2;
 } terceto;
+
+/* Variables externas */
+extern terceto lista_terceto[MAX_TERCETOS];
+extern int ultimo_terceto; /* Apunta al ultimo terceto escrito. Incrementarlo para guardar el siguiente. */
 
 #endif
