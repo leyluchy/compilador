50394cbb562c3f8373638dc117d3ddcf5a856676
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-11-02 18:17:12 -0300

    Genero tabla (falta resolver strings)

--- assembler.c
+++ assembler.c
@@ -9,6 +9,9 @@ void generarAssembler(){
 		return;
 	}
 
+  escribirInicio(arch);
+  generarTabla(arch);
+
   for(int i=0; i <= ultimo_terceto; i++){
     switch(lista_terceto[i].operador){
       case ASIG:
@@ -82,4 +85,32 @@ void generarAssembler(){
 
   fclose(arch);
 
+}
+
+void escribirInicio(FILE *arch){
+  fprintf(arch, "include macros2.asm\ninclude number.asm\n\n.MODEL SMALL\n.386\n.STACK 200h\n");
+}
+
+void generarTabla(FILE *arch){
+  fprintf(arch, ".DATA\n");
+
+  for(int i=0; i<=fin_tabla; i++){
+    fprintf(arch, "_%s dd ", tabla_simbolo[i].nombre);
+    switch(tabla_simbolo[i].tipo_dato){
+      case CteInt:
+      fprintf(arch, "%d\n", tabla_simbolo[i].valor_i);
+      break;
+
+      case CteFloat:
+      fprintf(arch, "%f\n", tabla_simbolo[i].valor_f);
+      break;
+
+      case CteString:
+      fprintf(arch, "%s\n", tabla_simbolo[i].valor_s);
+      break;
+
+      default: //Es una variable
+     fprintf(arch, "?\n");
+   }
+  }
 }
