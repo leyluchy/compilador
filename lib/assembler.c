6c03892e1b950016cda38d24059b46deecb2b1fe
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-11-04 17:50:10 -0300

    Escribo fin de asm, idento y limpio printf de prueba

--- lib/assembler.c
+++ lib/assembler.c
@@ -15,102 +15,85 @@ void generarAssembler(){
   for(int i=0; i <= ultimo_terceto; i++){
     switch(lista_terceto[i].operador){
       case ASIG:
- 			fprintf(arch,"case ASIG:");
-
-      break;
+        break;
       case CMP:
- 			fprintf(arch,"case CMP:");
-      break;
+        break;
 
       case BGT:
- 			fprintf(arch,"case BGT:");
-      break;
+            break;
       case BGE:
- 			fprintf(arch,"case BGE:");
-      break;
+        break;
       case BLT:
- 			fprintf(arch,"case BLT:");
-      break;
+        break;
       case BLE:
- 			fprintf(arch,"case BLE:");
-      break;
+        break;
       case BNE:
- 			fprintf(arch,"case BNE:");
-      break;
+        break;
       case BEQ:
- 			fprintf(arch,"case BEQ:");
-      break;
+        break;
       case JMP:
- 			fprintf(arch,"case JMP:");
-      break;
+        break;
 
       case THEN:
- 			fprintf(arch,"case THEN:");
-      break;
+        break;
       case ELSE:
- 			fprintf(arch,"case ELSE:");
-      break;
+        break;
       case ENDIF:
- 			fprintf(arch,"case ENDIF:");
-      break;
+        break;
 
       case WHILE:
- 			fprintf(arch,"case WHILE:");
-      break;
+        break;
       case ENDWHILE:
- 			fprintf(arch,"case ENDWHILE:");
-      break;
+        break;
 
       case MAS:
- 			fprintf(arch,"case MAS:");
-      break;
+        break;
       case MENOS:
- 			fprintf(arch,"case MENOS:");
-      break;
+        break;
       case POR:
- 			fprintf(arch,"case POR:");
-      break;
+        break;
       case DIVIDIDO:
- 			fprintf(arch,"case DIVIDIDO:");
-      break;
+        break;
 
       case READ:
- 			fprintf(arch,"case READ:");
-      break;
+        break;
       case WRITE:
- 			fprintf(arch,"case WRITE:");
-      break;
+        break;
     }
   }
 
+  escribirFinal(arch);
   fclose(arch);
 
 }
 
 void escribirInicio(FILE *arch){
-  fprintf(arch, "include macros2.asm\ninclude number.asm\n\n.MODEL SMALL\n.386\n.STACK 200h\n");
+  fprintf(arch, "include macros2.asm\ninclude number.asm\n\n.MODEL SMALL\n.386\n.STACK 200h\n\n");
+}
+
+void escribirFinal(FILE *arch){
+    fprintf(arch, "\nend\n");
 }
 
 void generarTabla(FILE *arch){
-  fprintf(arch, ".DATA\n");
+    fprintf(arch, ".DATA\n");
 
-  for(int i=0; i<=fin_tabla; i++){
-    fprintf(arch, "_%s dd ", tabla_simbolo[i].nombre);
-    switch(tabla_simbolo[i].tipo_dato){
-      case CteInt:
-      fprintf(arch, "%d\n", tabla_simbolo[i].valor_i);
-      break;
+    for(int i=0; i<=fin_tabla; i++){
+        fprintf(arch, "_%s dd ", tabla_simbolo[i].nombre);
+        switch(tabla_simbolo[i].tipo_dato){
+        case CteInt:
+            fprintf(arch, "%d\n", tabla_simbolo[i].valor_i);
+            break;
+        case CteFloat:
+            fprintf(arch, "%f\n", tabla_simbolo[i].valor_f);
+            break;
+        case CteString:
+            fprintf(arch, "%s\n", tabla_simbolo[i].valor_s);
+            break;
+        default: //Es una variable
+            fprintf(arch, "?\n");
+        }
+    }
 
-      case CteFloat:
-      fprintf(arch, "%f\n", tabla_simbolo[i].valor_f);
-      break;
-
-      case CteString:
-      fprintf(arch, "%s\n", tabla_simbolo[i].valor_s);
-      break;
-
-      default: //Es una variable
-     fprintf(arch, "?\n");
-   }
-  }
+    fprintf(arch, "\n");
 }
